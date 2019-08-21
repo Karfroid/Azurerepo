57ee34180d02eb417f10be4de0d425bee69068dc
karthik <karfroid@gmail.com>
2019-08-21 20:07:40 +0000

    test

--- file_holder/main.tf
+++ file_holder/main.tf
@@ -1,8 +1,28 @@
 resource "azurerm_resource_group" "demo" {
-  name     = "ThisisatestRG_TF"
+  name     = "ThisisatestRG_TF1"
   location = "West US"
 
   tags = {
     environment = "QA"
   }
 }
+
+resource "azurerm_resource_group" "demo" {
+  name     = "ThisisatestRG_TF2"
+  location = "West US"
+
+  tags = {
+    environment = "QA"
+  }
+}
+
+
+resource "azurerm_resource_group" "demo" {
+  name     = "ThisisatestRG_TF3"
+  location = "West US"
+
+  tags = {
+    environment = "QA"
+  }
+}
+
